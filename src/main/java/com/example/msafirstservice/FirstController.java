f651d9b9624ed38f5882c5e95202eb11a9722459
chaenabi <find1086@gmail.com>
2022-03-17 18:44:04 +0900

    port 0 set

--- src/main/java/com/example/msafirstservice/FirstController.java
+++ src/main/java/com/example/msafirstservice/FirstController.java
@@ -1,13 +1,24 @@
 package com.example.msafirstservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/first-service")
 public class FirstController {
+
+    private final Environment env;
+
+
     @GetMapping("/welcome")
     public String greeting() {
         return "first controller.";
@@ -19,7 +30,9 @@ public class FirstController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "check from first controller.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("check from first controller. port is %s", env.getProperty("local.server.port"));
     }
 }
